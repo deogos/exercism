02cfa662364cbddb0b53e65123d03d7b35932247
deogos <diogoalonso05@gmail.com>
2023-10-02 23:19:34 -0300

    2 java exercises completed

--- java/log-levels/src/main/java/LogLevels.java
+++ java/log-levels/src/main/java/LogLevels.java
@@ -1,14 +1,49 @@
 public class LogLevels {
-    
-    public static String message(String logLine) {
 
+    public static String message(String logLine) {
+        String cleanedLogLine = logLine.replaceAll("\\s+", " ").trim();
+
+        if (cleanedLogLine.startsWith("[ERROR]: ")) {
+            return cleanedLogLine.substring("[ERROR]: ".length());
+        } else if (cleanedLogLine.startsWith("[WARNING]: ")) {
+            return cleanedLogLine.substring("[WARNING]: ".length());
+        } else if (cleanedLogLine.startsWith("[INFO]: ")) {
+            return cleanedLogLine.substring("[INFO]: ".length());
+        }
+
+        return cleanedLogLine;
     }
+
 
     public static String logLevel(String logLine) {
-
-    }
+        String trimmedLogLine = logLine.trim();
+        if (trimmedLogLine.startsWith("[ERROR]: ")) {
+            return "error".toLowerCase();
+        } else if (trimmedLogLine.startsWith("[WARNING]: ")) {
+            return "warning".toLowerCase();
+        } else if (trimmedLogLine.startsWith("[INFO]: ")) {
+            return "info".toLowerCase();
+        }else {
+            return "";
+        }    }
 
     public static String reformat(String logLine) {
+        String trimmedLogLine = logLine.trim();
 
+        String logLevel = "";
+        String message = "";
+
+        if (trimmedLogLine.startsWith("[ERROR]: ")) {
+            logLevel = "error";
+            message = trimmedLogLine.substring("[ERROR]: ".length()).trim();
+        } else if (trimmedLogLine.startsWith("[WARNING]: ")) {
+            logLevel = "warning";
+            message = trimmedLogLine.substring("[WARNING]: ".length()).trim();
+        } else if (trimmedLogLine.startsWith("[INFO]: ")) {
+            logLevel = "info";
+            message = trimmedLogLine.substring("[INFO]: ".length()).trim();
+        }
+
+        return message + " (" + logLevel + ")";
     }
 }
